fa6a156991d571534be79ffefb55c85f70383b24
bialunelli <bialunelli11@gmail.com>
2026-04-06 16:45:43 -0300

    Serializers

--- core/serializers/livro.py
+++ core/serializers/livro.py
@@ -2,8 +2,20 @@ from rest_framework.serializers import ModelSerializer
 
 from core.models import Livro
 
+class LivroListSerializer(ModelSerializer):
+    class Meta:
+        model = Livro
+        fields = ('id', 'titulo', 'preco')
+        depth = 1
+
+class LivroRetrieveSerializer(ModelSerializer):
+    class Meta:
+        model = Livro
+        fields = '__all__'
+        depth = 1
 
 class LivroSerializer(ModelSerializer):
     class Meta:
         model = Livro
         fields = '__all__'
+        depth = 1
