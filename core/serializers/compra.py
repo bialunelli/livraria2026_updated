be1172463a53b6f22e5965c446ec01cafab802db
bialunelli <bialunelli11@gmail.com>
2026-06-23 15:10:25 -0300

    Adicionando

--- core/serializers/compra.py
+++ core/serializers/compra.py
@@ -4,8 +4,9 @@ from core.models import Compra
 
 
 class CompraSerializer(ModelSerializer):
-    status = CharField(source='get_status_display', read_only=True) # inclua essa linha
-    usuario = CharField(source='usuario.email', read_only=True)  # inclua essa linha
+    status = CharField(source='get_status_display', read_only=True)
+    usuario = CharField(source='usuario.email', read_only=True)
+    itens = ItensCompraSerializer(many=True, read_only=True)
 
     class Meta:
         model = Compra
